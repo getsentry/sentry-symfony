2873030eedf3a22eaeaee404086507914d057243
David Cramer <dcramer@gmail.com>
2016-07-21 12:25:40 -0700

    lint

--- src/Sentry/SentryBundle/SentryBundle.php
+++ src/Sentry/SentryBundle/SentryBundle.php
@@ -4,6 +4,7 @@ namespace Sentry\SentryBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class SentryBundle extends Bundle {
+class SentryBundle extends Bundle
+{
     const VERSION = '0.4.0';
 }
