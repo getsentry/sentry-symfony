58266d9a4ec474c57dbefa1b47014d278ffaf197
Ilya Kovalenko <agentsib@gmail.com>
2018-02-26 13:00:35 +0300

    Fix tests

--- test/DependencyInjection/SentryExtensionTest.php
+++ test/DependencyInjection/SentryExtensionTest.php
@@ -343,7 +343,12 @@ class SentryExtensionTest extends TestCase
             'install_shutdown_handler' => false,
             'processors' => ['processor1', 'processor2'],
             'processorOptions' => [
-                'processorOption1' => 'asasdf',
+                'processor1'    =>  [
+                    'processorOption1' => 'asasdf',
+                ],
+                'processor2'    =>  [
+                    'processorOption2' => 'asasdf',
+                ]
             ],
         ];
 
