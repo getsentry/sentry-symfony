8dc020077f0edb150aa4cf0ac49f78fcbba4d5b4
Helmut Januschka <helmut@januschka.com>
2018-03-12 15:22:38 +0100

    symfony version in 0.8.x (backport of #116)
    (cherry picked from commit d68d99e)

--- test/SentrySymfonyClientTest.php
+++ test/SentrySymfonyClientTest.php
@@ -20,7 +20,8 @@ class SentrySymfonyClientTest extends \PHPUnit_Framework_TestCase
     public function test_that_it_forwards_options()
     {
         $client = new SentrySymfonyClient('https://a:b@app.getsentry.com/project', array(
-            'name' => 'test'
+            'name' => 'test',
+            'tags' => array('some_custom' => 'test'),
         ));
 
         $data = $client->get_default_data();
@@ -28,6 +29,8 @@ class SentrySymfonyClientTest extends \PHPUnit_Framework_TestCase
         // Not a big fan of doing this kind of assertions, couples tests to external API...
         // Perhaps, refactor is needed for this class?
         $this->assertEquals('test', $data['server_name']);
+        $this->assertEquals(\Symfony\Component\HttpKernel\Kernel::VERSION, $data['tags']['symfony_version']);
+        $this->assertEquals('test', $data['tags']['some_custom']);
         $this->assertEquals('https://app.getsentry.com/api/project/store/', $client->getServerEndpoint(null));
         $this->assertEquals('a', $client->public_key);
         $this->assertEquals('b', $client->secret_key);
