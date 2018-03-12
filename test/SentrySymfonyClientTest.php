57d3919e97c36264407e4b0c66ddce3a87a096f4
Helmut Januschka <helmut@januschka.com>
2018-03-12 13:42:42 +0100

    cs fix

--- test/SentrySymfonyClientTest.php
+++ test/SentrySymfonyClientTest.php
@@ -31,7 +31,7 @@ class SentrySymfonyClientTest extends TestCase
         // Perhaps, refactor is needed for this class?
         $this->assertEquals('test', $data['server_name']);
         $this->assertEquals(\Symfony\Component\HttpKernel\Kernel::VERSION, $data['tags']['symfony_version']);
-+       $this->assertEquals('test', $data['tags']['some_custom']);
+        $this->assertEquals('test', $data['tags']['some_custom']);
         $this->assertEquals('https://app.getsentry.com/api/project/store/', $client->getServerEndpoint(null));
         $this->assertEquals('a', $client->public_key);
         $this->assertEquals('b', $client->secret_key);
