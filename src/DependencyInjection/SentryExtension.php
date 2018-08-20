aadc34442bb1794adebce3f8284bf06ae901e93f
Alex Rock Ancelet <pierstoval@gmail.com>
2018-08-20 11:28:57 +0200

    Move to XML services and remove Yaml dependency

--- src/DependencyInjection/SentryExtension.php
+++ src/DependencyInjection/SentryExtension.php
@@ -24,8 +24,8 @@ class SentryExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
 
         foreach ($config as $key => $value) {
             $container->setParameter('sentry.' . $key, $value);
