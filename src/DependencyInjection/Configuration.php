f3dedde08f6913f542cfe39a24c2981cba023d96
Alessandro Lai <alessandro.lai85@gmail.com>
2019-03-01 22:11:16 +0100

    Refactor Configuration to allow static analysis

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -24,17 +24,17 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('sentry');
         /** @var ArrayNodeDefinition $rootNode */
-        $rootNode = \method_exists(TreeBuilder::class, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('sentry');
+        $rootNode = \method_exists(TreeBuilder::class, 'getRootNode')
+            ? $treeBuilder->getRootNode()
+            : $treeBuilder->root('sentry');
 
         // Basic Sentry configuration
         $rootNode->children()
             ->scalarNode('dsn')
-                ->beforeNormalization()
-                    ->ifString()
-                    ->then($this->getTrimClosure())
-                ->end()
-                ->defaultNull()
-            ->end();
+            ->defaultNull()
+            ->beforeNormalization()
+            ->ifString()
+            ->then($this->getTrimClosure());
 
         // Options array (to be passed to Sentry\Options constructor) -- please keep alphabetical order!
         $optionsNode = $rootNode->children()
@@ -42,110 +42,85 @@ class Configuration implements ConfigurationInterface
             ->addDefaultsIfNotSet();
 
         $defaultValues = new Options();
+        $optionsChildNodes = $optionsNode->children();
 
-        $optionsNode
-            ->children()
-            ->booleanNode('attach_stacktrace')->end()
-            ->variableNode('before_breadcrumb')
-                ->validate()
-                    ->ifTrue($this->isNotAValidCallback())
-                    ->thenInvalid('Expecting callable or service reference, got %s')
-                ->end()
-            ->end()
-            ->variableNode('before_send')
-                ->validate()
-                    ->ifTrue($this->isNotAValidCallback())
-                    ->thenInvalid('Expecting callable or service reference, got %s')
-                ->end()
-            ->end()
-            ->booleanNode('default_integrations')->end()
-            ->integerNode('context_lines')
-                ->min(0)
-                ->max(99)
-            ->end()
-            ->booleanNode('enable_compression')->end()
-            ->scalarNode('environment')
-                ->defaultValue('%kernel.environment%')
-                ->cannotBeEmpty()
-            ->end()
-            ->scalarNode('error_types')
-            ->end()
-            ->arrayNode('in_app_exclude')
-                ->defaultValue([
-                    '%kernel.cache_dir%',
-                    $this->getProjectRoot() . '/vendor',
-                ])
-                ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('excluded_exceptions')
-                ->defaultValue($defaultValues->getExcludedExceptions())
-                ->prototype('scalar')->end()
-            ->end()
-            ->scalarNode('http_proxy')
-            ->end()
-            ->arrayNode('integrations')
-                ->prototype('scalar')
-                    ->validate()
-                        ->ifTrue(function ($value): bool {
-                            if (! is_string($value)) {
-                                return true;
-                            }
+        $optionsChildNodes->booleanNode('attach_stacktrace');
+        $optionsChildNodes->variableNode('before_breadcrumb')
+            ->validate()
+            ->ifTrue($this->isNotAValidCallback())
+            ->thenInvalid('Expecting callable or service reference, got %s');
+        $optionsChildNodes->variableNode('before_send')
+            ->validate()
+            ->ifTrue($this->isNotAValidCallback())
+            ->thenInvalid('Expecting callable or service reference, got %s');
+        $optionsChildNodes->integerNode('context_lines')
+            ->min(0)
+            ->max(99);
+        $optionsChildNodes->booleanNode('default_integrations');
+        $optionsChildNodes->booleanNode('enable_compression');
+        $optionsChildNodes->scalarNode('environment')
+            ->defaultValue('%kernel.environment%')
+            ->cannotBeEmpty();
+        $optionsChildNodes->scalarNode('error_types');
+        $optionsChildNodes->arrayNode('in_app_exclude')
+            ->defaultValue([
+                '%kernel.cache_dir%',
+                $this->getProjectRoot() . '/vendor',
+            ])
+            ->prototype('scalar');
+        $optionsChildNodes->arrayNode('excluded_exceptions')
+            ->defaultValue($defaultValues->getExcludedExceptions())
+            ->prototype('scalar');
+        $optionsChildNodes->scalarNode('http_proxy');
+        $optionsChildNodes->arrayNode('integrations')
+            ->prototype('scalar')
+            ->validate()
+            ->ifTrue(function ($value): bool {
+                if (! is_string($value)) {
+                    return true;
+                }
 
-                            return '@' !== substr($value, 0, 1);
-                        })
-                    ->thenInvalid('Expecting service reference, got %s')
-                    ->end()
-                ->end()
-            ->end()
-            ->scalarNode('logger')
-            ->end()
-            ->integerNode('max_breadcrumbs')
-                ->min(1)
-            ->end()
-            ->integerNode('max_value_length')
-                ->min(1)
-            ->end()
-            ->arrayNode('prefixes')
-                ->defaultValue($defaultValues->getPrefixes())
-                ->prototype('scalar')->end()
-            ->end()
-            ->scalarNode('project_root')
-                ->defaultValue($this->getProjectRoot())
-            ->end()
-            ->scalarNode('release')
-            ->end()
-            ->floatNode('sample_rate')
-                ->min(0.0)
-                ->max(1.0)
-            ->end()
-            ->integerNode('send_attempts')
-                ->min(1)
-            ->end()
-            ->booleanNode('send_default_pii')->end()
-            ->scalarNode('server_name')
-            ->end()
-            ->arrayNode('tags')
-                ->normalizeKeys(false)
-                ->prototype('scalar')
-            ->end()
-        ;
+                return '@' !== substr($value, 0, 1);
+            })
+            ->thenInvalid('Expecting service reference, got %s');
+        $optionsChildNodes->scalarNode('logger');
+        $optionsChildNodes->integerNode('max_breadcrumbs')
+            ->min(1);
+        $optionsChildNodes->integerNode('max_value_length')
+            ->min(1);
+        $optionsChildNodes->arrayNode('prefixes')
+            ->defaultValue($defaultValues->getPrefixes())
+            ->prototype('scalar');
+        $optionsChildNodes->scalarNode('project_root')
+            ->defaultValue($this->getProjectRoot());
+        $optionsChildNodes->scalarNode('release');
+        $optionsChildNodes->floatNode('sample_rate')
+            ->min(0.0)
+            ->max(1.0);
+        $optionsChildNodes->integerNode('send_attempts')
+            ->min(1);
+        $optionsChildNodes->booleanNode('send_default_pii');
+        $optionsChildNodes->scalarNode('server_name');
+        $optionsChildNodes->arrayNode('tags')
+            ->normalizeKeys(false)
+            ->prototype('scalar');
 
         // Bundle-specific configuration
-        $rootNode->children()
+        $listenerPriorities = $rootNode->children()
             ->arrayNode('listener_priorities')
-                ->addDefaultsIfNotSet()
-                ->children()
-                    ->scalarNode('request')->defaultValue(1)->end()
-                    ->scalarNode('console')->defaultValue(1)->end()
-                ->end()
-            ->end();
+            ->addDefaultsIfNotSet()
+            ->children();
+        $listenerPriorities->scalarNode('request')
+            ->defaultValue(1);
+        $listenerPriorities->scalarNode('console')
+            ->defaultValue(1);
 
         return $treeBuilder;
     }
 
-    private function getTrimClosure(): callable
+    private function getTrimClosure(): \Closure
     {
-        return function ($str) {
+        return function ($str): ?string {
             $value = trim($str);
             if ($value === '') {
                 return null;
