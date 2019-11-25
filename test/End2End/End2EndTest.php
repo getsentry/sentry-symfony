2ce05643942395fba91f99537c85a12b0c8fb792
Alessandro Lai <alessandro.lai85@gmail.com>
2019-11-25 15:42:45 +0100

    Add basic E2E test

---
+++ test/End2End/End2EndTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sentry\SentryBundle\Test\End2End;
+
+use Sentry\SentryBundle\Test\End2End\App\Kernel;
+use Sentry\State\HubInterface;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class End2EndTest extends WebTestCase
+{
+    protected function setUp(): void
+    {
+        static::$class = Kernel::class;
+        
+        parent::setUp();
+    }
+
+    protected static function getKernelClass(): string
+    {
+        return Kernel::class;
+    }
+
+    public function testGet404(): void 
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/missing-page');
+
+        $response = $client->getResponse();
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame(404, $response->getStatusCode(), $response->getContent());
+        
+        $hub = $client->getContainer()->get('test.hub');
+
+        $this->assertInstanceOf(HubInterface::class, $hub);
+        $this->assertNotNull($hub->getLastEventId(), 'Last error not captured');
+    }
+}
