9037f470344085f4f585318afbeea5587a1aa86f
Malcolm Fell <emarref@gmail.com>
2020-05-07 12:41:52 +1200

    CS space

--- src/EventListener/MessengerListener.php
+++ src/EventListener/MessengerListener.php
@@ -41,7 +41,7 @@ final class MessengerListener
 
         $error = $event->getThrowable();
 
-        if (!$error instanceof HandlerFailedException) {
+        if (! $error instanceof HandlerFailedException) {
             // All errors thrown during handling a command are captured by the HandleMessageMiddleware,
             // and are raised inside a HandlerFailedException. Type check for safety.
             return;
