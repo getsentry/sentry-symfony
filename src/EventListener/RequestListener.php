a683859401b15efbc9cfa91972ba76117477ce25
annuh <annevandevenis@gmail.com>
2020-07-08 11:23:34 +0200

    Fix PHP-CS issues

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -14,17 +14,17 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
 use Symfony\Component\Security\Core\User\UserInterface;
 
 if (Kernel::MAJOR_VERSION >= 5) {
-    if (!class_exists(RequestListenerRequestEvent::class, false)) {
+    if (! class_exists(RequestListenerRequestEvent::class, false)) {
         class_alias(RequestEvent::class, RequestListenerRequestEvent::class);
     }
-    if (!class_exists(RequestListenerControllerEvent::class, false)) {
+    if (! class_exists(RequestListenerControllerEvent::class, false)) {
         class_alias(ControllerEvent::class, RequestListenerControllerEvent::class);
     }
 } else {
-    if (!class_exists(RequestListenerRequestEvent::class, false)) {
+    if (! class_exists(RequestListenerRequestEvent::class, false)) {
         class_alias(GetResponseEvent::class, RequestListenerRequestEvent::class);
     }
-    if (!class_exists(RequestListenerControllerEvent::class, false)) {
+    if (! class_exists(RequestListenerControllerEvent::class, false)) {
         class_alias(FilterControllerEvent::class, RequestListenerControllerEvent::class);
     }
 }
