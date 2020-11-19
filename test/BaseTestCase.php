94c7dd6b6d62cb8febcc517b027615d842b091f6
W0rma <beck.worma@gmail.com>
2020-11-19 15:26:08 +0100

    Support PHPUnit 9 (#389)
    * Drop PHP 7.1
    
    * Drop phpunit 7.5 and allow phpunit 9 which adds support for PHP 8
    
    * Use ProphecyTrait if available - fixes deprecation warnings in phpunit 9
    
    * Replace assertContains() since its behaviour has changed in phpunit 9
    
    * Bump symfony/framework-bundle version to 3.4.23 in dev since this version makes the KernelTestCase compatible with newer versions of PHPUnit
    
    * Do not set symfony_constraint in prefer-lowest since this may overwrite our min dev dependencies

--- test/BaseTestCase.php
+++ test/BaseTestCase.php
@@ -3,13 +3,26 @@
 namespace Sentry\SentryBundle\Test;
 
 use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelInterface;
 
-abstract class BaseTestCase extends TestCase
+// Trait is available in phpspec/prophecy-phpunit:2.0 which requires at least PHP 7.3
+if (trait_exists(ProphecyTrait::class)) {
+    abstract class BaseProphecyTestCase extends TestCase
+    {
+        use ProphecyTrait;
+    }
+} else {
+    abstract class BaseProphecyTestCase extends TestCase
+    {
+    }
+}
+
+abstract class BaseTestCase extends BaseProphecyTestCase
 {
     protected function getSupportedOptionsCount(): int
     {
