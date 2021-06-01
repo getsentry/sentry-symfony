e14fc014e54447c1ff6814909f7723b765344ef8
Alessandro Lai <alessandro.lai85@gmail.com>
2021-06-01 19:36:34 +0200

    Make the TracingController emit a trace anyway

--- tests/End2End/App/Controller/TracingController.php
+++ tests/End2End/App/Controller/TracingController.php
@@ -5,26 +5,48 @@ declare(strict_types=1);
 namespace Sentry\SentryBundle\Tests\End2End\App\Controller;
 
 use Doctrine\DBAL\Connection;
+use Sentry\State\HubInterface;
+use Sentry\Tracing\SpanContext;
 use Symfony\Component\HttpFoundation\Response;
 
 class TracingController
 {
     /**
+     * @var HubInterface
+     */
+    private $hub;
+
+    /**
      * @var Connection|null
      */
     private $connection;
 
-    public function __construct(Connection $connection = null)
+    public function __construct(HubInterface $hub, Connection $connection = null)
     {
+        $this->hub = $hub;
         $this->connection = $connection;
     }
 
     public function pingDatabase(): Response
     {
+        $this->hub->setSpan(
+            $this->hub->getSpan()
+                ->startChild($this->createSpan())
+        );
+
         if ($this->connection) {
             $this->connection->executeQuery('SELECT 1');
         }
 
         return new Response('Success');
+    }
+
+    private function createSpan(): SpanContext
+    {
+        $spanContext = new SpanContext();
+        $spanContext->setOp('mock.span');
+        $spanContext->setDescription('mocked subspan');
+
+        return $spanContext;
     }
 }
