751864671fe1820b002cb8ad6671712532c3330d
Alessandro Lai <alessandro.lai85@gmail.com>
2021-07-07 23:25:37 +0200

    Fix DBAL v2 detection in tests

--- tests/DoctrineTestCase.php
+++ tests/DoctrineTestCase.php
@@ -6,6 +6,7 @@ namespace Sentry\SentryBundle\Tests;
 
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\DBAL\Driver;
+use Doctrine\DBAL\Driver\ResultStatement;
 use PHPUnit\Framework\TestCase;
 
 abstract class DoctrineTestCase extends TestCase
@@ -18,13 +19,13 @@ abstract class DoctrineTestCase extends TestCase
     protected static function isDoctrineDBALVersion2Installed(): bool
     {
         return self::isDoctrineDBALInstalled()
-            && !self::isDoctrineDBALVersion3Installed();
+            && interface_exists(ResultStatement::class);
     }
 
     protected static function isDoctrineDBALVersion3Installed(): bool
     {
         return self::isDoctrineDBALInstalled()
-            && !class_exists(Driver\ResultStatement::class);
+            && !self::isDoctrineDBALVersion2Installed();
     }
 
     protected static function isDoctrineBundlePackageInstalled(): bool
