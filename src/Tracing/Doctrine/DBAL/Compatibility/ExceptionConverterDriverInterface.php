0491c39eea42c003cc5f52997bade27d33cb5dbb
Alessandro Lai <alessandro.lai85@gmail.com>
2021-07-07 23:42:18 +0200

    Remove TODO

--- src/Tracing/Doctrine/DBAL/Compatibility/ExceptionConverterDriverInterface.php
+++ src/Tracing/Doctrine/DBAL/Compatibility/ExceptionConverterDriverInterface.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Sentry\SentryBundle\Tracing\Doctrine\DBAL\Compatibility;
 
 /**
- * TODO: can we remove this? We will need tests from #517 to check.
- *
  * @internal
  */
 interface ExceptionConverterDriverInterface
