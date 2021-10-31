c8510eea58ff96e73a0ae393f9952cda543f8e47
Alexander Schranz <alexander@sulu.io>
2021-10-31 17:43:02 +0100

    Update RequestListener.php

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -68,7 +68,7 @@ final class RequestListener
         }
 
         if (null !== $token
-            && (!method_exists($token, 'isAuthenticated') || $token->isAuthenticated(true))
+            && (!method_exists($token, 'isAuthenticated') || $token->isAuthenticated(false))
             && null !== $token->getUser()
         ) {
             $userData->setUsername($this->getUsername($token->getUser()));
