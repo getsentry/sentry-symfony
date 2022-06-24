69d167dd4b48ada2b45455e7012085275bee4cb4
Alessandro Lai <alessandro.lai85@gmail.com>
2022-06-24 09:34:05 +0200

    Remove or rename more test data providers

--- tests/EventListener/RequestListenerTest.php
+++ tests/EventListener/RequestListenerTest.php
@@ -17,7 +17,6 @@ use Sentry\State\Scope;
 use Sentry\UserDataBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Kernel;
@@ -51,12 +50,9 @@ final class RequestListenerTest extends TestCase
     }
 
     /**
-     * @dataProvider handleKernelRequestEventForSymfonyVersionAtLeast43DataProvider
-     * @dataProvider handleKernelRequestEventForSymfonyVersionLowerThan43DataProvider
-     *
-     * @param GetResponseEvent|RequestEvent $requestEvent
+     * @dataProvider handleKernelRequestEventDataProvider
      */
-    public function testHandleKernelRequestEvent($requestEvent, ?ClientInterface $client, ?TokenInterface $token, ?UserDataBag $expectedUser): void
+    public function testHandleKernelRequestEvent(RequestEvent $requestEvent, ?ClientInterface $client, ?TokenInterface $token, ?UserDataBag $expectedUser): void
     {
         $scope = new Scope();
 
@@ -85,121 +81,7 @@ final class RequestListenerTest extends TestCase
     /**
      * @return \Generator<mixed>
      */
-    public function handleKernelRequestEventForSymfonyVersionLowerThan43DataProvider(): \Generator
-    {
-        if (version_compare(Kernel::VERSION, '4.3.0', '>=')) {
-            return;
-        }
-
-        yield 'event.requestType != MASTER_REQUEST' => [
-            new GetResponseEvent(
-                $this->createMock(HttpKernelInterface::class),
-                new Request([], [], [], [], [], ['REMOTE_ADDR' => '127.0.0.1']),
-                HttpKernelInterface::SUB_REQUEST
-            ),
-            $this->getMockedClientWithOptions(new Options(['send_default_pii' => true])),
-            null,
-            null,
-        ];
-
-        yield 'options.send_default_pii = FALSE' => [
-            new GetResponseEvent(
-                $this->createMock(HttpKernelInterface::class),
-                new Request([], [], [], [], [], ['REMOTE_ADDR' => '127.0.0.1']),
-                HttpKernelInterface::MASTER_REQUEST
-            ),
-            $this->getMockedClientWithOptions(new Options(['send_default_pii' => false])),
-            null,
-            null,
-        ];
-
-        yield 'token IS NULL' => [
-            new GetResponseEvent(
-                $this->createMock(HttpKernelInterface::class),
-                new Request([], [], [], [], [], ['REMOTE_ADDR' => '127.0.0.1']),
-                HttpKernelInterface::MASTER_REQUEST
-            ),
-            $this->getMockedClientWithOptions(new Options(['send_default_pii' => true])),
-            null,
-            UserDataBag::createFromUserIpAddress('127.0.0.1'),
-        ];
-
-        yield 'token.authenticated = FALSE' => [
-            new GetResponseEvent(
-                $this->createMock(HttpKernelInterface::class),
-                new Request([], [], [], [], [], ['REMOTE_ADDR' => '127.0.0.1']),
-                HttpKernelInterface::MASTER_REQUEST
-            ),
-            $this->getMockedClientWithOptions(new Options(['send_default_pii' => true])),
-            new UnauthenticatedTokenStub(),
-            UserDataBag::createFromUserIpAddress('127.0.0.1'),
-        ];
-
-        yield 'token.authenticated = TRUE && token.user IS NULL' => [
-            new GetResponseEvent(
-                $this->createMock(HttpKernelInterface::class),
-                new Request([], [], [], [], [], ['REMOTE_ADDR' => '127.0.0.1']),
-                HttpKernelInterface::MASTER_REQUEST
-            ),
-            $this->getMockedClientWithOptions(new Options(['send_default_pii' => true])),
-            new AuthenticatedTokenStub(null),
-            UserDataBag::createFromUserIpAddress('127.0.0.1'),
-        ];
-
-        yield 'token.authenticated = TRUE && token.user INSTANCEOF string' => [
-            new GetResponseEvent(
-                $this->createMock(HttpKernelInterface::class),
-                new Request([], [], [], [], [], ['REMOTE_ADDR' => '127.0.0.1']),
-                HttpKernelInterface::MASTER_REQUEST
-            ),
-            $this->getMockedClientWithOptions(new Options(['send_default_pii' => true])),
-            new AuthenticatedTokenStub('foo_user'),
-            new UserDataBag(null, null, '127.0.0.1', 'foo_user'),
-        ];
-
-        yield 'token.authenticated = TRUE && token.user INSTANCEOF UserInterface && getUserIdentifier() method DOES NOT EXISTS' => [
-            new GetResponseEvent(
-                $this->createMock(HttpKernelInterface::class),
-                new Request([], [], [], [], [], ['REMOTE_ADDR' => '127.0.0.1']),
-                HttpKernelInterface::MASTER_REQUEST
-            ),
-            $this->getMockedClientWithOptions(new Options(['send_default_pii' => true])),
-            new AuthenticatedTokenStub(new UserWithoutIdentifierStub()),
-            new UserDataBag(null, null, '127.0.0.1', 'foo_user'),
-        ];
-
-        yield 'token.authenticated = TRUE && token.user INSTANCEOF UserInterface && getUserIdentifier() method EXISTS' => [
-            new GetResponseEvent(
-                $this->createMock(HttpKernelInterface::class),
-                new Request([], [], [], [], [], ['REMOTE_ADDR' => '127.0.0.1']),
-                HttpKernelInterface::MASTER_REQUEST
-            ),
-            $this->getMockedClientWithOptions(new Options(['send_default_pii' => true])),
-            new AuthenticatedTokenStub(new UserWithIdentifierStub()),
-            new UserDataBag(null, null, '127.0.0.1', 'foo_user'),
-        ];
-
-        yield 'token.authenticated = TRUE && token.user INSTANCEOF object && __toString() method EXISTS' => [
-            new GetResponseEvent(
-                $this->createMock(HttpKernelInterface::class),
-                new Request([], [], [], [], [], ['REMOTE_ADDR' => '127.0.0.1']),
-                HttpKernelInterface::MASTER_REQUEST
-            ),
-            $this->getMockedClientWithOptions(new Options(['send_default_pii' => true])),
-            new AuthenticatedTokenStub(new class() implements \Stringable {
-                public function __toString(): string
-                {
-                    return 'foo_user';
-                }
-            }),
-            new UserDataBag(null, null, '127.0.0.1', 'foo_user'),
-        ];
-    }
-
-    /**
-     * @return \Generator<mixed>
-     */
-    public function handleKernelRequestEventForSymfonyVersionAtLeast43DataProvider(): \Generator
+    public function handleKernelRequestEventDataProvider(): \Generator
     {
         if (version_compare(Kernel::VERSION, '4.3.0', '<')) {
             return;
@@ -324,7 +206,7 @@ final class RequestListenerTest extends TestCase
     }
 
     /**
-     * @dataProvider handleKernelControllerEventWithSymfonyVersionAtLeast43DataProvider
+     * @dataProvider handleKernelControllerEventDataProvider
      *
      * @param array<string, string> $expectedTags
      */
@@ -349,7 +231,7 @@ final class RequestListenerTest extends TestCase
     /**
      * @return \Generator<mixed>
      */
-    public function handleKernelControllerEventWithSymfonyVersionAtLeast43DataProvider(): \Generator
+    public function handleKernelControllerEventDataProvider(): \Generator
     {
         if (version_compare(Kernel::VERSION, '4.3.0', '<')) {
             return;
