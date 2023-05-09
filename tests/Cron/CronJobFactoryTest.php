86eaf7ce0a48eea333465a38f7143783b9eed92b
William Ihde <william@ihde.ch>
2023-05-09 08:00:24 +0200

    Add test.

---
+++ tests/Cron/CronJobFactoryTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sentry\SentryBundle\Tests\Cron;
+
+use PHPUnit\Framework\TestCase;
+use Sentry\SentryBundle\Cron\CronJobFactory;
+use Sentry\SentryBundle\Cron\CronJobInterface;
+
+class CronJobFactoryTest extends TestCase
+{
+    public function testCronJob()
+    {
+        // Setup test
+        $factory = new CronJobFactory('test', 'test-release');
+        $cronjob = $factory->getCronjob('test-monitor');
+        $this->assertInstanceOf(CronJobInterface::class, $cronjob);
+
+        // Create a CheckIn
+        $checkIn = $cronjob->inProgress();
+        $this->assertEquals('test', $checkIn->getEnvironment());
+        $this->assertEquals('test-release', $checkIn->getRelease());
+        $this->assertEquals('test-monitor', $checkIn->getMonitorSlug());
+    }
+}
