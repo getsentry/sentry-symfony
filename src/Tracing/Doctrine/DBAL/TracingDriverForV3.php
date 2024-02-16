a418b43cf82c1c6cdd75dccb9e0141b764d30441
Oleg Andreyev <oleg.andreyev@lampa.lv>
2024-02-16 22:57:42 +0200

    Use AbstractDriverMiddleware class instead of own implementation, by reusing common abstract class it's possible to unwind wrappedDriver

--- src/Tracing/Doctrine/DBAL/TracingDriverForV3.php
+++ src/Tracing/Doctrine/DBAL/TracingDriverForV3.php
@@ -6,10 +6,7 @@ namespace Sentry\SentryBundle\Tracing\Doctrine\DBAL;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver;
-use Doctrine\DBAL\Driver\API\ExceptionConverter;
-use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Schema\AbstractSchemaManager;
-use Doctrine\DBAL\VersionAwarePlatformDriver;
+use Doctrine\DBAL\Driver\Middleware\AbstractDriverMiddleware;
 
 /**
  * This is a simple implementation of the {@see Driver} interface that decorates
@@ -20,17 +17,12 @@ use Doctrine\DBAL\VersionAwarePlatformDriver;
  *
  * @phpstan-import-type Params from \Doctrine\DBAL\DriverManager as ConnectionParams
  */
-final class TracingDriverForV3 implements Driver, VersionAwarePlatformDriver
+final class TracingDriverForV3 extends AbstractDriverMiddleware
 {
     /**
      * @var TracingDriverConnectionFactoryInterface The connection factory
      */
     private $connectionFactory;
-
-    /**
-     * @var Driver|VersionAwarePlatformDriver The instance of the decorated driver
-     */
-    private $decoratedDriver;
 
     /**
      * Constructor.
@@ -40,8 +32,8 @@ final class TracingDriverForV3 implements Driver, VersionAwarePlatformDriver
      */
     public function __construct(TracingDriverConnectionFactoryInterface $connectionFactory, Driver $decoratedDriver)
     {
+        parent::__construct($decoratedDriver);
         $this->connectionFactory = $connectionFactory;
-        $this->decoratedDriver = $decoratedDriver;
     }
 
     /**
@@ -52,51 +44,9 @@ final class TracingDriverForV3 implements Driver, VersionAwarePlatformDriver
     public function connect(array $params): TracingDriverConnectionInterface
     {
         return $this->connectionFactory->create(
-            $this->decoratedDriver->connect($params),
-            $this->decoratedDriver->getDatabasePlatform(),
+            parent::connect($params),
+            $this->getDatabasePlatform(),
             $params
         );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getDatabasePlatform(): AbstractPlatform
-    {
-        return $this->decoratedDriver->getDatabasePlatform();
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @phpstan-template T of AbstractPlatform
-     *
-     * @phpstan-param T $platform
-     *
-     * @phpstan-return AbstractSchemaManager<T>
-     */
-    public function getSchemaManager(Connection $conn, AbstractPlatform $platform): AbstractSchemaManager
-    {
-        return $this->decoratedDriver->getSchemaManager($conn, $platform);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getExceptionConverter(): ExceptionConverter
-    {
-        return $this->decoratedDriver->getExceptionConverter();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function createDatabasePlatformForVersion($version): AbstractPlatform
-    {
-        if ($this->decoratedDriver instanceof VersionAwarePlatformDriver) {
-            return $this->decoratedDriver->createDatabasePlatformForVersion($version);
-        }
-
-        return $this->getDatabasePlatform();
     }
 }
