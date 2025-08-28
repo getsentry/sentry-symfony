31ca51bbb5f705ebb11f0571f7b7d4f1e5742d20
Martin Linzmayer <martin.linzmayer@sentry.io>
2025-08-28 17:34:28 +0200

    change to inheritance

--- src/Monolog/LogsHandler.php
+++ src/Monolog/LogsHandler.php
@@ -4,39 +4,24 @@ declare(strict_types=1);
 
 namespace Sentry\SentryBundle\Monolog;
 
-use Monolog\Formatter\FormatterInterface;
-use Monolog\Handler\HandlerInterface;
 use Monolog\Logger as MonologLogger;
 use Monolog\LogRecord;
 use Sentry\Monolog\CompatibilityLogLevelTrait;
 use Sentry\Monolog\LogsHandler as BaseLogsHandler;
 
 /**
- * Wrapper for the Sentry LogsHandler that uses Monolog constants for initialization.
- * Sentry's LogLevel classes cannot be properly used in Symfony configuration so this acts as
- * a service facade that can be instantiated using yml/xml config files.
+ * Extends the base LogsHandler so that it can be used with Monolog constants.
+ * Sentry LogLevel objects are not easily usable with symfony config files, so this provides
+ * a convenient way to instantiate the service in yaml/xml.
  */
-class LogsHandler implements HandlerInterface
+class LogsHandler extends BaseLogsHandler
 {
     use CompatibilityLogLevelTrait;
-
-    /**
-     * @var BaseLogsHandler
-     */
-    private $logsHandler;
 
     public function __construct(int $level = MonologLogger::DEBUG, bool $bubble = true)
     {
         $logLevel = self::getSentryLogLevelFromMonologLevel($level);
-        $this->logsHandler = new BaseLogsHandler($logLevel, $bubble);
-    }
-
-    /**
-     * @param array<string, mixed>|LogRecord $record
-     */
-    public function isHandling($record): bool
-    {
-        return $this->logsHandler->isHandling($record);
+        parent::__construct($logLevel, $bubble);
     }
 
     /**
@@ -46,49 +31,10 @@ class LogsHandler implements HandlerInterface
     {
         // Extra check required here because `isHandling` is not guaranteed to
         // be called, and we might accidentally capture log messages that should be filtered.
-        if ($this->logsHandler->isHandling($record)) {
-            return $this->logsHandler->handle($record);
+        if ($this->isHandling($record)) {
+            return parent::handle($record);
         }
 
         return false;
-    }
-
-    /**
-     * @param array<array<string, mixed>|LogRecord> $records
-     */
-    public function handleBatch(array $records): void
-    {
-        $this->logsHandler->handleBatch($records);
-    }
-
-    public function close(): void
-    {
-        $this->logsHandler->close();
-    }
-
-    /**
-     * @param callable $callback
-     */
-    public function pushProcessor($callback): void
-    {
-        $this->logsHandler->pushProcessor($callback);
-    }
-
-    /**
-     * @return callable
-     */
-    public function popProcessor()
-    {
-        return $this->logsHandler->popProcessor();
-    }
-
-    public function setFormatter(FormatterInterface $formatter): void
-    {
-        $this->logsHandler->setFormatter($formatter);
-    }
-
-    public function getFormatter(): FormatterInterface
-    {
-        return $this->logsHandler->getFormatter();
     }
 }
