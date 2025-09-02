617d03ea375cd885090e90fecc4a09302c167c05
Martin Linzmayer <martin.linzmayer@sentry.io>
2025-09-02 10:45:10 +0200

    fix(logs): use correct option name (#941)

---
+++ tests/End2End/App/Callback/BeforeSendLogCallback.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sentry\SentryBundle\Tests\End2End\App\Callback;
+
+use Sentry\Logs\Log;
+
+class BeforeSendLogCallback
+{
+    public function getCallback(): callable
+    {
+        return function (Log $log): ?Log {
+            if ('before_send_log' === $log->getBody()) {
+                return null;
+            }
+
+            return $log;
+        };
+    }
+}
